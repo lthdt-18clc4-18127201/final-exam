390056149db325899f08bf1740d57673c04fa9e1
su pham <phamtansu2000@gmail.com>
2024-01-14 18:18:44 -0800

    add API logger and save to database

---
+++ backend/middlewares/logger.mdw.js
@@ -0,0 +1,22 @@
+import logger from '../utils/logger/logger.js';
+
+export default function(req, res, next) {
+    res.on('finish', () => {
+        if(res.statusCode >= 400) {
+            switch(req.originalUrl){
+                case '/api/auth/login': {
+                    logger.error(
+                    `Status: ${res.statusCode} - ${req.method}: ${req.originalUrl}: Enter wrong password`);
+                    break
+                };
+                case '/api/auth/register': {logger.error(
+                    `Status: ${res.statusCode} - ${req.method}: ${req.originalUrl}: Register process has an occured`);
+                    break
+                };
+            }
+            
+        } 
+    });
+
+    next();
+}
